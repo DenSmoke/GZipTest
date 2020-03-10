a6a77e4bb42d950a9ddb1e7deb2bef39089de8e2
Денис Перевозчиков <densmoke@live.ru>
2020-03-11 02:25:54 +0300

    Added GZipChunkFactory and GZipChunk with compression capability

---
+++ GZipTest/GZipChunk.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.IO.Compression;
+
+namespace GZipTest
+{
+
+    public sealed class GZipChunk
+    {
+        private readonly GZipChunkFactory _factory;
+
+        internal GZipChunk(int number, long start, long end, GZipChunkFactory factory)
+        {
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            Number = number;
+            Start = start;
+            End = end;
+        }
+
+        public void Process()
+        {
+            switch (_factory.Operation)
+            {
+                case Operation.Compress: Compress(); break;
+                case Operation.Decompress: Decompress(); break;
+                default: break;
+            }
+        }
+
+        public int Number { get; }
+
+        public long Start { get; }
+
+        public long End { get; }
+
+        public string FilePartPath { get; private set; }
+
+        private void Compress()
+        {
+            FilePartPath = Path.Combine(_factory.TempPath, $"part_{Number}.gz");
+
+            using var input = new FileStream(_factory.InputFile, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan)
+            {
+                Position = Start
+            };
+            using var output = new FileStream(FilePartPath, FileMode.Create, FileAccess.Write, FileShare.None, 4096, FileOptions.SequentialScan);
+            using var gz = new GZipStream(output, CompressionMode.Compress);
+
+            var pool = ArrayPool<byte>.Shared;
+            var bytesToRead = (int)(End - Start);
+            var bufferSize = Math.Min(_factory.MaxBufferSize, bytesToRead);
+            var buffer = pool.Rent(bufferSize);
+
+            try
+            {
+                do
+                {
+                    var bytesRead = 0;
+                    var bytesToReadToBuffer = Math.Min(bytesToRead, bufferSize);
+                    do
+                    {
+                        bytesRead += input.Read(buffer, bytesRead, bytesToReadToBuffer);
+                        bytesToReadToBuffer -= bytesRead;
+                    }
+                    while (bytesToReadToBuffer > 0);
+
+                    gz.Write(buffer, 0, bytesRead);
+
+                    bytesToRead -= bytesRead;
+                }
+                while (bytesToRead > 0);
+            }
+            finally
+            {
+                pool.Return(buffer);
+            }
+        }
+
+        private void Decompress() => throw new NotImplementedException();
+
+    }
+}
---
+++ GZipTest/GZipChunkFactory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace GZipTest
+{
+    public sealed class GZipChunkFactory : IDisposable
+    {
+        private static readonly PerformanceCounter _ramCounter = new PerformanceCounter("Memory", "Available Bytes");
+        private bool _disposed;
+
+        public GZipChunkFactory()
+        {
+            TempPath = Path.Combine(Path.GetTempPath(), $"GZipChunks_{Guid.NewGuid()}");
+            Directory.CreateDirectory(TempPath);
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                Directory.Delete(TempPath, true);
+                _disposed = true;
+            }
+        }
+
+        public string TempPath { get; }
+
+        public string InputFile { get; set; }
+
+        public Operation Operation { get; set; }
+
+        public int HeaderSize { get; private set; }
+
+        public int MaxBufferSize { get; private set; }
+
+        public IEnumerable<GZipChunk> Create()
+        {
+            if (!File.Exists(InputFile))
+                throw new InvalidOperationException("Input file does not exists");
+
+            MaxBufferSize = (int)Math.Min(_ramCounter.NextValue() * 0.8f / Environment.ProcessorCount, int.MaxValue);
+            if (MaxBufferSize < 1024 * 1024)
+                throw new InsufficientMemoryException("Not enough memory");
+
+            return Operation switch
+            {
+                Operation.Compress => CreateForCompression(),
+                Operation.Decompress => CreateForDecompression(),
+                _ => throw new InvalidOperationException("Unknown operation"),
+            };
+        }
+
+        private IEnumerable<GZipChunk> CreateForCompression()
+        {
+            var fileSize = new FileInfo(InputFile).Length;
+            var processors = Environment.ProcessorCount;
+            var chunkSize = (int)Math.Min(Math.Round((double)fileSize / processors), int.MaxValue);
+            var chunksCount = (int)(fileSize / chunkSize) + 1;
+            HeaderSize = 4 + chunksCount * 8;
+
+            long start = HeaderSize + 1;
+            for (var i = 0; i < chunksCount - 1; i++)
+            {
+                var end = start + chunkSize;
+                yield return new GZipChunk(i, start, end, this);
+                start = end + 1;
+            }
+            yield return new GZipChunk(chunksCount, start, fileSize, this);
+        }
+
+        private IEnumerable<GZipChunk> CreateForDecompression() => throw new NotImplementedException();
+    }
+}
