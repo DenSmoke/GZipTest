c423706f3732f84db91f6bff4c2c6cde7217c00d
Денис Перевозчиков <densmoke@live.ru>
2020-03-16 02:45:32 +0300

    Reworked GZipChunkFactory and implemented CreateForDecompression

--- GZipTest/GZipChunkFactory.cs
+++ GZipTest/GZipChunkFactory.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.MemoryMappedFiles;
 
 namespace GZipTest
 {
-    internal sealed class GZipChunkFactory
+    internal sealed class GZipChunkFactory : IDisposable
     {
+        private MemoryMappedFile _mmf;
+        private const int GZIP_HEADER_SIZE = 10;
+        private long _fileSize;
 
-        public GZipChunkFactory()
+        public GZipChunkFactory() => ChunksCount = Environment.ProcessorCount;
+
+        public void Dispose()
         {
-            BufferSize = 8192;
-            ChunksCount = Environment.ProcessorCount;
+            if (_mmf is object)
+            {
+                _mmf.Dispose();
+                _mmf = null;
+            }
         }
-
-        public int BufferSize { get; set; }
 
         public string InputFile { get; set; }
 
@@ -23,11 +30,11 @@ namespace GZipTest
 
         public IEnumerable<GZipChunk> Create()
         {
-            if (!File.Exists(InputFile))
+            var fileInfo = new FileInfo(InputFile);
+            if (!fileInfo.Exists)
                 throw new InvalidOperationException("Input file does not exists");
-            if (BufferSize < 1)
-                throw new InvalidOperationException("Invalid buffer size");
 
+            _fileSize = fileInfo.Length;
             return Operation switch
             {
                 Operation.Compress => CreateForCompression(),
@@ -38,10 +45,11 @@ namespace GZipTest
 
         private IEnumerable<GZipChunk> CreateForCompression()
         {
-            var fileSize = new FileInfo(InputFile).Length;
-            var div = fileSize / ChunksCount;
-            var rem = fileSize % ChunksCount;
+            Dispose();
+            _mmf = MemoryMappedFile.CreateFromFile(InputFile, FileMode.Open, Path.GetFileName(InputFile), 0, MemoryMappedFileAccess.Read);
 
+            var div = _fileSize / ChunksCount;
+            var rem = _fileSize % ChunksCount;
             long start = 0;
             for (var i = 0; i < ChunksCount; i++)
             {
@@ -51,11 +59,61 @@ namespace GZipTest
                     chunkSize++;
                     rem--;
                 }
-                yield return new GZipChunk(BufferSize, InputFile, Operation, start, chunkSize);
+                yield return new GZipChunk(_mmf, Operation, start, chunkSize);
                 start += chunkSize;
             }
         }
 
-        private IEnumerable<GZipChunk> CreateForDecompression() => throw new NotImplementedException();
+        private IEnumerable<GZipChunk> CreateForDecompression()
+        {
+            Dispose();
+            _mmf = MemoryMappedFile.CreateFromFile(InputFile, FileMode.Open, Path.GetFileName(InputFile), 0, MemoryMappedFileAccess.Read);
+
+            var blockIndices = GetBlockIndices();
+            for (var i = 0; i < blockIndices.Count; i++)
+            {
+                var start = blockIndices[i];
+                var chunkSize = i + 1 < blockIndices.Count ? blockIndices[i + 1] - start : _fileSize - start;
+                yield return new GZipChunk(_mmf, Operation, start, chunkSize);
+            }
+        }
+
+        public List<long> GetBlockIndices()
+        {
+            //using var input = new FileStream(InputFile, FileMode.Open, FileAccess.Read, FileShare.Read, BufferSize, FileOptions.SequentialScan);
+            using var input = _mmf.CreateViewStream(0, 0, MemoryMappedFileAccess.Read);
+
+            var header = new byte[GZIP_HEADER_SIZE];
+            var bytesRead = 0;
+            var bytesToRead = GZIP_HEADER_SIZE;
+            do
+            {
+                var n = input.Read(header, bytesRead, bytesToRead);
+                bytesRead += n;
+                bytesToRead -= n;
+            }
+            while (bytesToRead > 0);
+
+            var blockIndices = new List<long>() { 0 };
+            var patternLength = header.Length;
+            var matchCount = 0;
+
+            int _byte;
+            while ((_byte = input.ReadByte()) != -1)
+            {
+                if (_byte == header[matchCount])
+                {
+                    matchCount++;
+                    if (matchCount == patternLength)
+                    {
+                        blockIndices.Add(input.Position - patternLength);
+                        matchCount = 0;
+                    }
+                }
+                else
+                    matchCount = 0;
+            }
+            return blockIndices;
+        }
     }
 }
