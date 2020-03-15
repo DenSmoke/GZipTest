327d7fd6d81ff00210a2f79e045e286e9a427c85
Денис Перевозчиков <densmoke@live.ru>
2020-03-15 20:53:07 +0300

    Modified Program

--- GZipTest/Program.cs
+++ GZipTest/Program.cs
@@ -1,41 +1,36 @@
 ﻿using System;
-using System.IO;
 
 namespace GZipTest
 {
 
-    internal class Program
+    public static class Program
     {
         /// <summary>
         ///     Entry point
         /// </summary>
         /// <param name="args">Command line arguments: compress/decompress input_file output_file</param>
-        private static int Main(string[] args)
+        public static int Main(string[] args)
         {
             try
             {
-                if (args.Length < 3)
-                    throw new InvalidOperationException("Not enough arguments");
+                if (args?.Length != 3)
+                    throw new InvalidOperationException("Invalid parameters");
 
                 var operation = args[0];
                 var inputFile = args[1];
                 var outputFile = args[2];
 
-                if (!File.Exists(inputFile))
-                    throw new FileNotFoundException("Input file is not found");
-
-                var compressor = new GZipCompressor();
                 switch (operation)
                 {
-                    case "compress": compressor.Compress(inputFile, outputFile); break;
-                    case "decompress": compressor.Decompress(inputFile, outputFile); break;
+                    case "compress": GZipCompressor.Compress(inputFile, outputFile); break;
+                    case "decompress": GZipCompressor.Decompress(inputFile, outputFile); break;
                     default: throw new InvalidOperationException("Invalid \"operation\" argument");
                 }
                 return 0;
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex);
                 return 1;
             }
         }
